e1af604fb57138f8317a0a5651a14d7048f26449
bopdidus <bopdidus@gmail.com>
2022-03-06 23:14:59 +0100

    Creation of helpers class

--- src/Model/Talk.java
+++ src/Model/Talk.java
@@ -1,6 +1,40 @@
 package Model;
 
+import Utils.Verify;
+
 public class Talk {
     private String title;
     private int time;
+
+    public Talk(String _title, int _time) throws Exception{
+        if(Verify.ContainsDigit(_title)==true)
+            throw new Exception("The title should not contains a digit");
+        if(_title.isEmpty())
+            throw new Exception("The title should not be empty");
+        this.title = title;
+        if(Verify.CheckLigthningFive(_time)!=true)
+            throw new Exception("The time should be in 5 lightning");
+        this.time = _time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) throws Exception {
+        if(Verify.ContainsDigit(title)!=true)
+            throw new Exception("The title should not contains a digit");
+        this.title = title;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) throws Exception {
+        if(Verify.CheckLigthningFive(time)!=true)
+            throw new Exception("The title should not contains a digit");
+        this.time = time;
+    }
+
 }
---
+++ src/Test/VerifyTest.java
@@ -0,0 +1,59 @@
+package Test;
+
+import Model.Track;
+import Utils.Verify;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VerifyTest{
+        private Track track;
+
+        @BeforeEach
+        public void CreateInstance()
+        {
+                track = new Track();
+        }
+
+        @Test
+        public void ContainsDigitTest()
+        {
+                String test = "jfglj44s1gg";
+
+                assertTrue(Verify.ContainsDigit(test));
+        }
+
+        @Test
+        public void ContainsDigitTest2()
+        {
+                String test = "eifufsdjfjsd";
+
+                assertFalse(Verify.ContainsDigit(test));
+        }
+
+        @Test
+        public void CheckLigthningFiveTest()
+        {
+                assertTrue(Verify.CheckLigthningFive(25));
+        }
+
+
+        @Test
+        public void CheckLigthningFiveTest2()
+        {
+                assertFalse(Verify.CheckLigthningFive(13));
+        }
+
+        @Test
+        public void CheckHoursMorningTest()
+        {
+                assertEquals(180, Verify.CheckHoursMorning(track));
+        }
+
+        @Test
+        public void CheckHourNetworkingEventTest()
+        {
+                int somme = Verify.CheckHourNetworkingEvent(track);
+                assertTrue((somme <= 240 && 180 <= somme));
+        }
+}
